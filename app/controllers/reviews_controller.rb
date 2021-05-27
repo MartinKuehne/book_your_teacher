61e9c79f0d5f11b08346a28d5d41cf692001ff79
Martin Kuehne <martinkuehne@live.de>
2021-05-27 12:40:58 +0200

    half way through reviews and dashboard

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,34 @@
+class ReviewsController < ApplicationController
+  before_action :set_review, only: [:destroy]
+  before_action :set_booking, only: [:create]
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.booking = @booking
+    flash[:notice] = @review.errors.full_messages.to_sentence unless @review.save
+    redirect_to dashboard_path
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :comment)
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:booking_id])
+  end
+end
